039ad600397488a4f1e70d59b8ea61c4a159cbd8
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-02-24 02:11:04 +0200

    1 file add

---
+++ js.js
@@ -0,0 +1,16 @@
+function array(arr){
+     arr.sort((a,b) => {
+        if( a > b){
+            return 1;}
+        if( a < b){
+            return -1;
+        }
+        else {
+            return 0
+        }
+        })
+    console.log(arr)
+    arr.splice(1,4)
+    return arr
+}
+console.log(array([1,2,3,4,9,21,6,55,11]))
